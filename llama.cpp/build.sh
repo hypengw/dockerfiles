fef726faa8daef462b5f745be349d9c885dddd4a
hypengw <hypengwip@gmail.com>
2025-09-20 22:59:29 +0800

    support nvidia vulkan on llama.cpp

--- llama.cpp/build.sh
+++ llama.cpp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-podman build --security-opt label=disable -f Dockerfile.sycl -t llama.cpp:sycl -v /opt/intel:/opt/intel:ro
-podman build --security-opt label=disable -f Dockerfile.vulkan -t llama.cpp:vulkan
+#podman build --security-opt label=disable -f Dockerfile.sycl -t llama.cpp:sycl -v /opt/intel:/opt/intel:ro
+podman build --security-opt label=disable -f Dockerfile.vulkan -t llama.cpp:vulkan --network=host
